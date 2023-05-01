d3707d8da6388a33dba5c75356ad579578b764a3
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-05-01 17:58:41 +0530

    first

---
+++ hello.js
@@ -0,0 +1,16 @@
+var name="sudeep"
+var n=10
+var n1=new Number(11)
+
+var name1=new String("suitable")
+
+console.log(name1)
+
+var s=true
+var s2=new Boolean(true)
+
+k=20
+
+console.log(k)
+
+console.log("heelo world sudeep "+n+" name: "+name)
