e476e00b63ab680addadcf8bf9e1fb4aee8a2687
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-05-03 20:36:59 +0530

    module from local file

---
+++ modulesample.js
@@ -0,0 +1,5 @@
+// import module
+var h = require('./sample');     //importing the module sample.js
+
+console.log(h.hey)
+h.hello()
---
+++ sample.js
@@ -0,0 +1,5 @@
+
+module.exports.hello = function() {//exporting the function hello
+    console.log("hey friends")
+}
+module.exports.hey=10//exporting the variable hey
