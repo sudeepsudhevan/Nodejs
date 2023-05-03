90bed8f817bd0ab24bf653e82a4f45ffd40b2f42
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-05-03 16:58:45 +0530

    constvariable

---
+++ constvariable.js
@@ -0,0 +1,16 @@
+
+const hello=100//const is used to declare a constant variable,its value can't be changed
+
+//hello=200 error because we are changing the value of constant variable
+
+
+
+const hey=["sai","kumar","remanan"]//we can change the values of array and object
+
+// hey=["sai","kumar","reddy"]   error because we are changing the whole array
+
+
+
+hey.push("reddy")//we can add new elements to the array
+
+console.log(hey)
