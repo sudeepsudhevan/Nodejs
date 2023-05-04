41dcf8c7fd27f6354a03ef84f582fe1cb9f92ca3
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-05-04 19:22:16 +0530

    server page html

--- sampleserver.js
+++ sampleserver.js
@@ -2,17 +2,42 @@
 
 var http=require('http'); //http is a module which is used to create a server
 var fs=require('fs');   //fs is a module which is used to read and write files
+var url=require('url'); //url is a module which is used to parse the url
 
 http.createServer(function(req,res){//callback function,here req and res are parameters
 
+    var q=url.parse(req.url);//parse is used to parse the url
+    //console.log(q.pathname);
+
+    if(q.pathname=="/"){//pathname is used to get the path of the url
+    
 
     fs.readFile("scientific.html",function(err,data){//callback function,here err and data are parameters
         res.writeHead(200,{'Content-Type':'text/html'});//200 is the status code,writeHead is used to write the header of the response
         res.write(data)
         res.end();
-    
+   
     });
-}).listen(3000);//listen is used to listen to a port,here 3000 is the port number
+    }else if(q.pathname==="/signup"){
+    fs.readFile("signup.html",function(err,data){    
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write(data);
+        res.end();
+    });    
+    }else if(q.pathname==="/signupaction"){
+
+    res.write('action')
+    res.end()
+
+
+    }else{
+    res.writeHead(404,{'Content-Type':'text/html'});
+    res.write("error");
+    res.end();
+}
+}).listen(3000,function(){
+    console.log("server started");
+});//listen is used to listen to a port,here 3000 is the port number
 
 
 
